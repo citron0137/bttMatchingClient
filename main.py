e8a8af1e5a241dc64b44180b25737fd80fa703e6
citron0137 <citron0137@gmail.com>
2018-12-11 15:41:55 -0800

    fasdfsadfadsf

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ import iframe_extraction
 import save_hash
 import compare_hash
 
-def getList(stateNumber):
+def getList(n):
+    stateNumber=n
     masterApi = apiHandler.Rest()
     torrents = masterApi.get("torrent_file")
    
@@ -33,51 +34,57 @@ def getList(stateNumber):
     return torrentList
 
 def doWork(torrentSeq):
-    #setting
-    masterApi = apiHandler.Rest()
-    
-    os.mkdir('tmp')
-    os.mkdir('tmp/torrent')
-    os.mkdir('tmp/subFiles')
-    os.mkdir('tmp/frames')
-    
-#start
-    masterApi.updateTorrentState(torrentSeq, 'analyzing')   # torrent status update
-    torrentFilePath = masterApi.downloadTorrentBySeq(torrentSeq, './tmp/torrent/')# torrent file download
-    print(torrentFilePath)
-    torrentFileName = torrentFilePath.split("/")[-1]
-
-    #TODO download via torrent
-    progress=0
-    with open('downloadSubFile.log','a+') as f:
-        progress = torrentDownloader.downloadSubFile('tmp/torrent/', torrentFilePath.split('/')[-1], 'tmp/subFiles',f)
-    print(progress)
-
-    #TODO find video file in downloaded files
-    videos=[]
-    videos.append(find_video.find_video('tmp/subFiles/'))
-    print(videos)
-
-    
-    for video in videos:
-        #TODO upload sub_file
-        masterApi.insertSubfile(torrentSeq,video[0],video[3],video[2])
-
-        #TODO extract frame from video file
-        iframe_extraction.iframe_extract(video[1]+'/'+video[0],'tmp/frames')
+    try:
+        #setting
+        masterApi = apiHandler.Rest()
         
-        #TODO extract hash from framfile
-        save_hash.hashing('tmp/frames'+'/'+video[0])
+        os.mkdir('tmp')
+        os.mkdir('tmp/torrent')
+        os.mkdir('tmp/subFiles')
+        os.mkdir('tmp/frames')
+        
+    #start
+        masterApi.updateTorrentState(torrentSeq, 'analyzing')   # torrent status update
+        torrentFilePath = masterApi.downloadTorrentBySeq(torrentSeq, './tmp/torrent/')# torrent file download
+        print(torrentFilePath)
+        torrentFileName = torrentFilePath.split("/")[-1]
 
-        #TODO matching frame hash wiht original hash #TODO uploade matched data
-        compare_hash.compare('tmp/frames'+'/"'+video[0]+'"/"'+video[0]+'".hash')
+        #TODO download via torrent
+        progress=0
+        with open('downloadSubFile.log','a+') as f:
+            progress = torrentDownloader.downloadSubFile('tmp/torrent/', torrentFilePath.split('/')[-1], 'tmp/subFiles',f)
+        print(progress)
 
+        #TODO find video file in downloaded files
+        videos=[]
+        videos.append(find_video.find_video('tmp/subFiles/'))
+        print(videos)
+
+        
+        for video in videos:
+            #TODO upload sub_file
+            masterApi.insertSubfile(torrentSeq,video[0],video[3],video[2])
+
+            #TODO extract frame from video file
+            iframe_extraction.iframe_extract(video[1]+'/'+video[0],'tmp/frames')
+            
+            #TODO extract hash from framfile
+            save_hash.hashing('tmp/frames'+'/'+video[0])
+
+            #TODO matching frame hash wiht original hash #TODO uploade matched data
+            compare_hash.compare('tmp/frames'+'/"'+video[0]+'"/"'+video[0]+'".hash')
+    except Exception as ex:
+        f = open('error.log','a+')
+        f.write("SEQ : "+torrentSeq +"\n"+ str(ex)+"\n\n" )
+        pass
         
 
     #TODO delete folder
     os.system('rm -rf tmp')
 
 if __name__ == '__main__':
+    os.system('rm -rf tmp')
+    stateNumber = 9 ##modify
     torrents = getList(stateNumber) #get torrent seq
     for torrent in torrents:
-        doWork(torrent['seq'])
+        doWork(str(torrent['seq']))
